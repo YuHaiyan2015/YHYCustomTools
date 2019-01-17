205305b86049492e9f617b4d034f6ad1264b0102
girlCoding <yuhaiyan@yuhaiyandeMacBook-Pro.local>
2019-01-17 17:41:11 +0800

    test

---
+++ LogTools/NSArray+Log.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+Log.h
+//  XPMicroShop
+//
+//  Created by laizhijin on 16/10/13.
+//  Copyright © 2016年 Simplex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Log)
+
+@end
+
+@interface NSDictionary (Log)
+
+@end
